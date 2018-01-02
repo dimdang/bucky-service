4f4b952419acf971fb14397cbe67fc99149b7529
d.dim <d.dim@gl-f.com>
2018-01-02 17:11:40 +0700

    study about oauth2

---
+++ src/main/java/com/bucky/service/BuckyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bucky.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckyServiceApplication.class, args);
+	}
+}
